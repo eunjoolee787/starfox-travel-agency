97bcf167356b2c5ec8381c8c9115c46aac544071
Eun Joo <eunjoolee787@gmail.com>
2014-12-18 19:55:41 -1000

    jade templating created

---
+++ app/server.js
@@ -0,0 +1,36 @@
+//INIT AND LIBRARIES
+var express = require('express');
+var app = express();
+
+//MIDDLEWARRE
+app.use(express.static('public'));
+app.set('view engine', 'jade');
+
+//app.engine('html', require('ejs').renderFile);
+app.get('/', function(req, res) {
+
+  var locals = {
+    myname : "EJL"
+  };
+//});
+
+//ROUTES
+//app.get('/', function(req, res) {
+
+  // res.render('./index', {
+  //   myname : "jon",
+
+    locals.mypets = [
+      "mochi",
+      "mochi",
+      "boba", 
+      "cookie",
+      "cat"
+    ];
+//   });
+// });
+
+res.render ('./index', locals);
+});
+//EXPORT THIS FILE AS A MODULE
+module.exports.app = app;
